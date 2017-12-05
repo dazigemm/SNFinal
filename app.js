6fb26916b27f9e995fc4b6305d16a4d678ae06a2
D <dazigemm@gmail.com>
2017-12-05 00:09:48 -0500

    added user authentication

--- app.js
+++ app.js
@@ -8,9 +8,17 @@ const publicPath = path.resolve(__dirname, 'public');
 app.use(express.static(publicPath));
 
 app.set('view engine', 'hbs');
-/*
+//*
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
+
+const session = require('express-session');
+const sessionOptions = {
+	secret: 'super secret',
+	resave: true,
+	saveUnitialized: true
+};
+app.use(session(sessionOptions));
 
 require('./db');
 const mongoose = require('mongoose');
@@ -21,7 +29,7 @@ const LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-app.use(passport.intialize());
+app.use(passport.initialize());
 app.use(passport.session());
 
 /* *** */
@@ -29,12 +37,42 @@ app.use(passport.session());
 app.get('/', function(req, res) {
 
 	//res.send('hello');
-	res.render('index', {greeting: 'salutations'});
+	res.render('index', {greeting: 'Salutations', user: req.user});
 });
 
-app.get('/faq', function(req, res) {
-	res.send('you has q, i has answer');
+/************* User Authentication Stuff *****************/
+
+app.get('/login', function (req, res) {
+	res.render('login', {user: req.user});
 });
+
+app.post('/login', passport.authenticate('local'), function(req, res) {
+	res.redirect('/');
+});
+
+app.get('/register', function(req, res) {
+	res.render('register');
+});
+
+app.post('/register', function(req, res) {
+	const pw = req.body.password;
+	const name = req.body.username;
+	User.register(new User({username: name}), pw, function(err, user) {
+		if (err) {
+			return res.render('register', {user: user});
+		}
+		passport.authenticate('local')(req, res, function () {
+			res.redirect('/');
+		});
+	});
+});
+
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+/***************************************************************************/
 
 app.listen(3000);
 console.log('Started server on port 3000');
--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 //const URLSlugs = require('mongoose-url-slugs');
-//const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const User = new mongoose.Schema({
 	name: String,
 	password: String
 });
 
+User.plugin(passportLocalMongoose);
+
+mongoose.model('User', User);
 
 mongoose.connect('mongodb://localhost/chefy');
