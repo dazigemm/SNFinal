6bda6f5c432813fb996ed4ba5900beb244dbfadd
D <dazigemm@gmail.com>
2017-12-06 15:55:24 -0500

    changed port

--- db.js
+++ db.js
@@ -28,29 +28,18 @@ User.plugin(passportLocalMongoose);
 
 mongoose.model('User', User);
 mongoose.model('Meal', Meal);
-//*
-if(process.env.NODE_ENV === 'PRODUCTION') {
-	var fs = require('fs');
-	var path = require('path');
-	var fn = path.join(__dirname, 'config.json');
-	var data = fs.readFileSync(fn);
 
-	var conf = JSON.parse(data);
-	var dbconf = conf.dbconf;
-}
-else {
-	dbconf = 'mongodb://localhost/chefy';
-}
+var uristring = process.env.MONGOLAB_URI || 
+	process.env.MONGOHQ_URL ||
+	'mongodb://localhost/HelloMongoose';
 
-mongoose.connect(dbconf);
-//*/
-/*
+var theport = process.env.PORT || 5000;
+
 mongoose.connect(uristring, function (err, res) {
 	if (err) {
-		console.log('ERROR connecting to: ' + uristring + '. '  err);
+		console.log('ERROR connecting to: ' + uristring + '- ' + err);
 	}
 	else {
-		console.long('Succeeded to connect to : " + uristring);
-		}
-	});
-*/
+		console.log('Succeeded connected to: ' + uristring);
+	}
+});
